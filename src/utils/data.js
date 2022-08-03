375624c167365b556d189e88f6a7696c57b86277
Nguyen Vu <nguyenvu191000@gmail.com>
2022-08-03 10:19:23 +0700

    Update

---
+++ src/utils/data.js
@@ -0,0 +1,212 @@
+export const MENU_DATAS = [
+  {
+    name: "Thời sự",
+    children: [
+      {
+        name: "Chính trị",
+      },
+      {
+        name: "Dân sinh",
+      },
+      {
+        name: "Lao động - Việc làm",
+      },
+      {
+        name: "Giao thông",
+      },
+      {
+        name: "Mekong",
+      },
+      {
+        name: "Quỹ Hy vọng",
+      },
+    ],
+  },
+  {
+    name: "Góc nhìn",
+    children: [
+      {
+        name: "Bình luận nhiều",
+      },
+      {
+        name: "Covid 19",
+      },
+      {
+        name: "Chính trị & Chính sách",
+      },
+      {
+        name: "Y tế & Sức khỏe",
+      },
+      {
+        name: "Kinh doanh và Quản trị",
+      },
+      {
+        name: "Giáo dục & Tri thức",
+      },
+      {
+        name: "Môi trường",
+      },
+      {
+        name: "Văn hóa & Lối sống",
+      },
+      {
+        name: "Tác giả",
+      },
+    ],
+  },
+  {
+    name: "Thế giới",
+    children: [
+      {
+        name: "Tư liệu",
+      },
+      {
+        name: "Phân tích",
+      },
+      {
+        name: "Người Việt 5 châu",
+      },
+      {
+        name: "Cuộc sống đó đây",
+      },
+      {
+        name: "Quân sự",
+      },
+    ],
+  },
+  {
+    name: "Video",
+    children: [
+      {
+        name: "Thời sự",
+      },
+      {
+        name: "Nhịp sống",
+      },
+      {
+        name: "Food",
+      },
+      {
+        name: "Tôi kể",
+      },
+    ],
+  },
+  {
+    name: "Podcasts",
+    children: [
+      {
+        name: "Vnexpress hôm nay",
+      },
+      {
+        name: "Li hôn",
+      },
+      {
+        name: "Thầm thì",
+      },
+    ],
+  },
+  {
+    name: "Kinh doanh",
+    children: [],
+  },
+  {
+    name: "Khoa học",
+    children: [],
+  },
+  {
+    name: "Giải trí",
+    children: [],
+  },
+  {
+    name: "Thể thao",
+    children: [],
+  },
+  {
+    name: "Pháp Luật",
+    children: [],
+  },
+  {
+    name: "Giáo dục",
+    children: [],
+  },
+  {
+    name: "Sức Khỏe",
+    children: [],
+  },
+  {
+    name: "Đời sống",
+    children: [],
+  },
+  {
+    name: "Du lịch",
+    children: [],
+  },
+  {
+    name: "Số hóa",
+    children: [],
+  },
+  {
+    name: "Xe",
+    children: [],
+  },
+  {
+    name: "Ý kiến",
+    children: [],
+  },
+  {
+    name: "Tâm sự",
+    children: [],
+  },
+  {
+    name: "Hài",
+    children: [],
+  },
+];
+
+export const postDatas = [
+  {
+    title: "Những hẻm Sài Gòn trước và sau khi người dân hiến đất mở rộng",
+    desc: "Nhiều hẻm trước đây đủ một xe máy đi lọt được người dân hiến đất mở rộng lên từ 4 đến 6 m giúp đi lại thuận tiện, khang trang hơn.",
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/fa5110960ef0ccae95e1-1-1657960572.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=FwQ6s2bGIq0_kYl0yO81dw",
+  },
+  {
+    title: "Cách phương Tây đối phó cuộc chiến dài hơi của ông Putin",
+    desc: "Ông Putin tin rằng cuộc chiến kéo dài ở Ukraine sẽ khiến đối phương kiệt quệ, nhưng phương Tây có thể có nhiều cách đối phó với tính toán này.",
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/tong-thong-nga-jpeg-8925-16578-1593-5886-1657906113.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=F0h3GKCoyhccBOGw9UCGDQ",
+  },
+  {
+    title: "Vũ khí phương Tây khó xoay chuyển chiến trường Ukraine",
+    desc: "Pháo phản lực phương Tây đang phát huy ưu thế ở Ukraine, song khó giúp Kiev lật ngược thế cờ trước Nga do số lượng quá ít.",
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/123-3366-1657945038.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=TaDMWbk2FLa7-HPdfu1BPg",
+  },
+  {
+    title: "Cựu tổng thống Sri Lanka: 'Tôi đã cố hết sức ngăn khủng hoảng'",
+    desc: 'Cựu tổng thống Gotabaya Rajapaksa viết trong đơn xin từ chức rằng ông "đã làm mọi cách có thể" để ngăn khủng hoảng.',
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/2022-07-12T233400Z-1982277787-6623-7888-1657959592.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=PZrqwvIq3hdpEo1keIGo5Q",
+  },
+  {
+    title: "HLV Pau đòi bỏ trận giao hữu giữa chừng",
+    desc: "Chứng kiến trọng tài rút hai thẻ đỏ cho Pau FC ở trận giao hữu với Angouleme, HLV Didier Tholot kêu gọi học trò ra ngoài sân tẩy chay.",
+    img: "https://i1-thethao.vnecdn.net/2022/07/16/pau-fc-jpeg-1657958688-6077-1657959237.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=9qjbeUhDvhNMh2w5W-m1IA",
+  },
+  {
+    title: "Để con trai ngồi trên mui ôtô đang phóng nhanh",
+    desc: "Cho con trai ngồi trên mui xe đang chạy nhanh, nữ tài xế bị CSGT phạt 4 triệu đồng, tước bằng lái hai tháng. ",
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/muixe-7966-1657944875.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=h6zMlGHvvrW5JRg6gZkywg",
+  },
+  {
+    title: "Tàu chiến Mỹ áp sát Trường Sa",
+    desc: "Mỹ thông báo điều tàu khu trục USS Benfold áp sát quần đảo Trường Sa để thực thi quyền tự do hàng hải trên Biển Đông.",
+    img: "https://i1-vnexpress.vnecdn.net/2022/07/16/uss-benfold-JPG-9321-1657949091.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=5coHO6GZENNTlYnK6woBjw",
+  },
+  {
+    title: "19 ngày chinh phục ngôi á hậu Miss Supranational của Kim Duyên",
+    desc: "Kim Duyên đến Ba Lan một mình, tự trang điểm cho các phần thi, vượt qua nhiều sự cố để đoạt ngôi á hậu Miss Supranational 2022 sau 19 ngày. ",
+    img: "https://i1-giaitri.vnecdn.net/2022/07/16/A-hau-Kim-Duyen-9-5807-1657961815.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=kn0IA6wMCCaw70Kign205Q",
+  },
+  {
+    title: "Vì sao Bitcoin vẫn đứng vững trước nhiều tin xấu?",
+    desc: "Giá Bitcoin vẫn duy trì trên 20.000 USD, trái ngược hoàn toàn với diễn biến thường gặp mỗi khi thị trường tiền ...",
+    img: "https://i1-kinhdoanh.vnecdn.net/2022/07/16/img-8850-jpg-1657939954-4749-1657939998.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=Dut90EVaLMRTO8QLNRyPKw",
+  },
+];
